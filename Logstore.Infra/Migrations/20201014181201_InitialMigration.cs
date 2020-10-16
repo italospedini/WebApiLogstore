6d6cbe5733951f5fe015a72010e5fc7356e29ab3
italospedini <italospedini@yahoo.com.br>
2020-10-15 23:21:11 -0300

    migration with initial data

--- Logstore.Infra/Migrations/20201014181201_InitialMigration.cs
+++ Logstore.Infra/Migrations/20201014181201_InitialMigration.cs
@@ -25,6 +25,28 @@ namespace Logstore.Infra.Migrations
                 {
                     table.PrimaryKey("PK_PizzaSabores", x => x.Id);
                 });
+
+            // Insert data to PizzaSabores Table
+            migrationBuilder.InsertData("PizzaSabores", new string[] { "Id", "NomeSabor", "Disponivel", "Ativa", "PrecoUnitario" },
+                new object[] { 1, "3 Queijos", 1, 1, 50.0 });
+
+            migrationBuilder.InsertData("PizzaSabores", new string[] { "Id", "NomeSabor", "Disponivel", "Ativa", "PrecoUnitario" },
+                new object[] { 2, "Frango com requeijão", 1, 1, 59.99 });
+
+            migrationBuilder.InsertData("PizzaSabores", new string[] { "Id", "NomeSabor", "Disponivel", "Ativa", "PrecoUnitario" },
+                new object[] { 3, "Mussarela", 1, 1, 42.5 });
+
+            migrationBuilder.InsertData("PizzaSabores", new string[] { "Id", "NomeSabor", "Disponivel", "Ativa", "PrecoUnitario" },
+                new object[] { 4, "Calabresa", 1, 1, 42.5 });
+
+            migrationBuilder.InsertData("PizzaSabores", new string[] { "Id", "NomeSabor", "Disponivel", "Ativa", "PrecoUnitario" },
+                new object[] { 5, "Pepperoni", 1, 1, 55.0 });
+
+            migrationBuilder.InsertData("PizzaSabores", new string[] { "Id", "NomeSabor", "Disponivel", "Ativa", "PrecoUnitario" },
+                new object[] { 6, "Portuguesa", 1, 1, 45.0 });
+
+            migrationBuilder.InsertData("PizzaSabores", new string[] { "Id", "NomeSabor", "Disponivel", "Ativa", "PrecoUnitario" },
+                new object[] { 7, "Veggie", 1, 1, 59.99 });
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
