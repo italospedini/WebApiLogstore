1ed48b3eaf376336246d811cfceaf1acc4585f5a
italospedini <italospedini@yahoo.com.br>
2020-10-15 23:31:36 -0300

    adding missing file

---
+++ Logstore.Api/DatabaseCreator.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Logstore.Api
+{
+    public static class DatabaseCreator
+    {
+        /// <summary>  
+        /// Migrates the database.  
+        /// </summary>  
+        /// <typeparam name="T"></typeparam>  
+        /// <param name="webHost">The web host.</param>  
+        /// <returns>IWebHost.</returns>  
+        public static IWebHost CreateDatabase<T>(this IWebHost webHost) where T : DbContext
+        {
+            using (var scope = webHost.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var db = services.GetRequiredService<T>();
+                    db.Database.Migrate();
+                }
+                catch (Exception ex)
+                {
+
+                }
+            }
+            return webHost;
+        }
+    }
+}
